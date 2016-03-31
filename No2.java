8e5c5a4fc62331c1df630be4c52ebe76498c0b4a
Ghina Alya Noviana <forevergan@rocketmail.com>
2016-03-31 11:07:19 +0700

    Added files via upload

---
+++ No2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.pkg2;
+
+/**
+ *
+ * @author Praktikum
+ */
+public class No2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Program Faktorial");
+        System.out.println("Masukkan Angka : 4");
+        System.out.println("   **   ");
+        System.out.println("  ****  ");
+        System.out.println(" ****** ");
+        System.out.println("********");
+        System.out.println("********");
+        System.out.println(" ****** ");
+        System.out.println("  ****  ");
+        System.out.println("   **   ");
+    }
+    
+}
